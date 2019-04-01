3d74eb8bb2305dd0a792ec0422f7c9367c1c57a9
sin3point14 <manaschaudhary2000@gmail.com>
2019-04-01 09:00:13 +0530

    finished landing page

--- index.php
+++ index.php
@@ -9,36 +9,54 @@
     body,html{
         padding: 0;
         margin:0;
+        background-color: #000098;
+        color: yellow;
     }
     #content{
         text-align: center;
         position: absolute;
-        top: 50%;
+        top: 40%;
         margin-top: -150px;
         width: 100vw;
     }
-    #image{
-        height: 200px;
-        width: 200px;
+    #img{
+        height: 300px;
+        width: 300px;
         border: 1px solid black;
         display: inline-block;
     }
-    
+    marquee{
+        font-size:50px;
+        font-family: 'Comic Sans MS';
+        margin-top: 30px;
+    }
+    .star{
+        height:200px;
+        width:200px;
+    }
+    #stars{
+        position:absolute;
+        display:inline-block;
+        bottom:30px;
+        width:100vw;
+        height:200px;
+        padding:0;
+        margin:0;
+        text-align:center;
+    }
 </style>
 </head>
 <body>
-    <div id="content">
-    <img id="img" src="#"/>
-    <input id="up" type="file" />
+    <div id="stars"></div>
+    <marquee behaviour="alternate" scrollamount=50>BABA JI KI JA HO</marquee>
+    <form id="content">
+    <img id="img" align="middle" src="#" onerror="this.src='http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder.png'"/><br><br>
+    <input id="up" type="file" /><br><br>
 
         <input type="text" placeholder="Name"/>
-    </div>
+        <input type='submit'/>
+</form>
 <script>
-	var jcrop_api;
-	var canvas;
-	var context;
-	var image;
-	var prefsize;
 	var up = document.getElementById('up');
 	up.onchange = function(){
 		var image = up.files[0];
@@ -46,16 +64,25 @@
 		var reader = new FileReader();
 		reader.onloadend = function(){
 			console.log(reader.result);
-			img = document.createElement('img');
+			img = document.getElementById('img');
 			img.src= reader.result;  //read.result contains the base64 code. Voh seedha src me laga ke bhi chal jata hai.Isko variable me store karva ke send kar de server side
-			var li = document.createElement('li');
+			//var li = document.createElement('li');
 			//preview ke liye canvas use kar le,it'll be much more efficient
-			li.appendChild(img);
-			document.getElementById('list').appendChild(li);
+			//li.appendChild(img);
+			//document.getElementById('list').appendChild(li);
 		}
 		reader.readAsDataURL(image); //this reads the base64 of the image. Iske upar vala block iske baad execute hota hai
 
 	}
+    var y=document.getElementById('stars');
+    var a= window.innerWidth/200;
+    a=parseInt(a);
+    var star = '<img class="star" src="https://thumbs.gfycat.com/GlassWellgroomedIndri.webp">';
+    
+    for(i=0;i<a;i++){
+        y.innerHTML+=star;
+    }
+
 </script>
 </body>
 </html>
