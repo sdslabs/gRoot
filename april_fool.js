97baad1419f3d025af71918c8a51b4b2479614da
sin3point14 <manaschaudhary2000@gmail.com>
2019-04-02 02:56:17 +0530

    final changes

--- april_fool.js
+++ april_fool.js
@@ -24,7 +24,11 @@ function loadermodchecker() {
   else
     loadermodcaller();
 }*/
-
+var quote=['The number is as random as the time you spent here',
+  'The number represents your standards - 404 Not found',
+  'The randomness of the number is just like your disappointment - Constant',
+  'Wow! you just generated a random number , Your loved ones would be so proud of you',
+  'This app is less disappointing then the time you spent here' ]
 function loadermod() {
   document.getElementById("fabtopper_loader").style.opacity="0";
   document.getElementById("akhtar_left").style.left="-60vw";
@@ -42,5 +46,30 @@ function delay () {
     x.style.zIndex="9999";
     setTimeout( function(){ 
       x.style.opacity="1";
-    }, 2000);
+      setTimeout( function(){ 
+        x.style.opacity="0";
+        document.getElementById('button').style.display='none';
+        document.getElementById('welcomeDiv').style.display='block';
+        document.getElementById('quote').style.display='block';
+        document.getElementById('randomNo').style.display='block';
+        document.getElementById('quote').innerHTML=quote[Math.floor(Math.random()*5)];
+        setTimeout( function(){ 
+          x.style.zIndex="-1000";
+          document.getElementById("akhtar_left").style.left="-60vw";
+          document.getElementById("akhtar_right").style.right="-60vw ";
+            setTimeout( function(){ 
+              x.style.zIndex="-1000";
+              document.getElementById('welcomeDiv').style.opacity='1';
+              setTimeout( function(){ 
+                x.style.zIndex="-1000";
+                document.getElementById('randomNo').style.opacity='1';
+                setTimeout( function(){ 
+                  x.style.zIndex="-1000";
+                  document.getElementById('quote').style.opacity='1';
+                }, 1100);
+              }, 1100);
+            }, 1100);       
+          }, 1500);
+      }, 5500);
+    }, 1000);
 }
